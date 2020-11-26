27155438e2aa542da3c4755347ff794f1d1f917d
Christopher Bourlon <christopher.bourlon@akqa.com>
2020-11-26 10:47:14 +0100

    test cms

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -3,8 +3,11 @@ import Link from 'next/link'
 import styles from '../Navigation/Navigation.module.scss'
 import Image from 'next/image'
 import cx from 'classnames';
+import { attributes } from '../../content/menu/index.md';
 
 export default function Navigation() {
+    let { menuLinks } = attributes;
+    console.log('menuLinks', menuLinks)
     return (
         <>
             <div className={styles.navigation}>
@@ -18,18 +21,32 @@ export default function Navigation() {
                     </Link>
 
                     {/* map over the links coming from content */}
+                    {/* useRouter?? */}
+
+
 
                     <div className={styles.main_menu}>
                         <ul className={styles.main_menu_list}>
-                            <li className={styles.main_menu_list_item}>
-                                <Link href="/prestations">
-                                    <a className={styles.link} aria-label="menu-nos prestations" href="/prestations/">
-                                        <span>Nos prestations</span>
-                                    </a>
-                                </Link>
-                            </li>
+                            {
+                                menuLinks.length !== 0 &&
+                                menuLinks.map(item => {
+                                    console.log('item', item)
+                                    return (
+                                        <li className={styles.main_menu_list_item} key={item.slug}>
+                                            <Link href={`/${item.slug}`}>
+                                                <a className={styles.link} aria-label={item.menuLink}>
+                                                    <span>{item.menuLink}</span>
+                                                </a>
+                                            </Link>
+                                        </li>
+                                    )
+                                })
+                            }
 
-                            <li className={styles.main_menu_list_item}>
+
+
+
+                            {/* <li className={styles.main_menu_list_item}>
                                 <Link href="/atouts">
                                     <a className={styles.link} aria-label="menu-nos atouts" href="/atouts/">
                                         <span>Nos atouts</span>
@@ -59,7 +76,7 @@ export default function Navigation() {
                                         <span>Contact</span>
                                     </a>
                                 </Link>
-                            </li>
+                            </li> */}
                         </ul>
                     </div>
 
---
+++ content/menu/index.md
@@ -0,0 +1,15 @@
+---
+title: menu
+date: 2019-03-17T19:31:20.591Z
+menuLinks:
+  - menuLink: "Nos prestations"
+    slug: "nos-prestations"
+  - menuLink: "Nos atouts"
+    slug: "nos-atouts"
+  - menuLink: "Notre vision"
+    slug: "notre-vision"
+  - menuLink: "Inspiration"
+    slug: "inspiration"
+  - menuLink: "Contact"
+    slug: "contact"
+---
