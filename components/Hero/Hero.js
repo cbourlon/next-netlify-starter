8d400372d28be6557076976ae0e7d6c629211855
Christopher Bourlon <christopher.bourlon@akqa.com>
2020-11-27 17:32:04 +0100

    test image

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -30,7 +30,8 @@ export default function Hero({ title, text, isHomePage, bgImageSrc }) {
         <>
             <div className={heroClass}>
                 <Image
-                    src={bgImageSrc || `/img/home-1600.jpg`}
+                    // src={bgImageSrc || `/img/home-1600.jpg`}
+                    src="/img/home-1600.jpg"
                     alt="illustration jardin"
                     layout='fill'
                     onLoad={loaded}
